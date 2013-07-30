4a1d1e32b895d237fa661251e9a3c2d20f732a92
Gaudenz Alder <gaudenz@jgraph.com>
2013-07-30 07:18:59 +0100

    2.1.0.6 release

--- php/src/mxServer.php
+++ php/src/mxServer.php
@@ -5,15 +5,15 @@
  *
  * Class: mxServer
  *
- * Bootstrapping for the PHP backend. This is version 2.1.0.5
+ * Bootstrapping for the PHP backend. This is version 2.1.0.6
  * of mxGraph.
  *
  * Variable: MXGRAPH-VERSION
  *
  * Constant that holds the current mxGraph version. The current version
- * is 2.1.0.5.
+ * is 2.1.0.6.
  */
-define("MXGRAPH-VERSION", "2.1.0.5");
+define("MXGRAPH-VERSION", "2.1.0.6");
 
 include_once("util/mxLog.php");
 include_once("util/mxConstants.php");
