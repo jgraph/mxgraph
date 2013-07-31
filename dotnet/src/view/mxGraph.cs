789d1942ced30926fc0a1d381fc25fbd6bc55c1c
Gaudenz Alder <gaudenz@jgraph.com>
2013-07-31 13:02:57 +0100

    2.1.0.7 release

--- dotnet/src/view/mxGraph.cs
+++ dotnet/src/view/mxGraph.cs
@@ -15,9 +15,9 @@ namespace com.mxgraph
     {
         /// <summary>
         /// Holds the version number of this release. Current version
-        /// is 2.1.0.6.
+        /// is 2.1.0.7.
         /// </summary>
-        public const String VERSION = "2.1.0.6";
+        public const String VERSION = "2.1.0.7";
 
         /// <summary>
         /// Holds the model that contains the cells to be displayed.
--- java/src/com/mxgraph/view/mxGraph.java
+++ java/src/com/mxgraph/view/mxGraph.java
@@ -195,9 +195,9 @@ public class mxGraph extends mxEventSource
 
 	/**
 	 * Holds the version number of this release. Current version
-	 * is 2.1.0.6.
+	 * is 2.1.0.7.
 	 */
-	public static final String VERSION = "2.1.0.6";
+	public static final String VERSION = "2.1.0.7";
 
 	/**
 	 * 
