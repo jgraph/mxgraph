bdbec46994bb563cb601de3e0220573c87872d3d
Gaudenz Alder <gaudenz@jgraph.com>
2013-09-25 13:03:03 +0100

    2.2.0.1 release

--- java/src/com/mxgraph/view/mxGraph.java
+++ java/src/com/mxgraph/view/mxGraph.java
@@ -195,9 +195,9 @@ public class mxGraph extends mxEventSource
 
 	/**
 	 * Holds the version number of this release. Current version
-	 * is 2.2.0.0.
+	 * is 2.2.0.1.
 	 */
-	public static final String VERSION = "2.2.0.0";
+	public static final String VERSION = "2.2.0.1";
 
 	/**
 	 * 
